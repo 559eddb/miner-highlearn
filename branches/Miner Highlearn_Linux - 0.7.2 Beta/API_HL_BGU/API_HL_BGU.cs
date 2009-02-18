092e2ad5f5d8950d3e36329c39aae152ec3114cf
wolf1986 <wolf1986@8d3ddbfa-fb97-11dd-984d-ab748d24aa7e>
2009-02-18 11:44:47 +0000

    Changed the name of the folder

--- branches/Miner Highlearn_Linux - 0.7.2 Beta/API_HL_BGU/API_HL_BGU.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Miner_Highlearn {
-	public class API_HL_BGU : API_Highlearn {
-		public API_HL_BGU() : base("hl2.bgu.ac.il") {
-			link_global_login = "http://hl2.bgu.ac.il/sso/login2.asp";
-		}
-	}
-}
--- branches/Miner Highlearn_Linux - 0.7.2 Beta/API_HL_BIU/API_HL_BIU.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace Miner_Highlearn {
-	public class API_HL_BIU : API_Highlearn {
-		public API_HL_BIU() : base("hl2.biu.ac.il") {
-		}
-	}
-}
