867e70e316845bcc65e9e1331cffcdc6d028ca2a
wolf1986 <wolf1986@8d3ddbfa-fb97-11dd-984d-ab748d24aa7e>
2009-02-19 13:53:03 +0000

    The Mono version is a branch of the main project

---
+++ API_HL_BGU/API_HL_BGU.cs
@@ -0,0 +1,12 @@
+// Code by Wolf1986 - Willy Fenchenko
+// License: GPL v3
+
+using System;
+
+namespace Miner_Highlearn {
+	public class API_HL_BGU : API_Highlearn {
+		public API_HL_BGU() : base("hl2.bgu.ac.il") {
+			link_global_login = "http://hl2.bgu.ac.il/sso/login2.asp";
+		}
+	}
+}
